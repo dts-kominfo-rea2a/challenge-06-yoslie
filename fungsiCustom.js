e5d10f80faf8ed46eb58d54685cf17e7d7908c51
Yoslie <yoslie.tan@gmail.com>
2022-06-12 06:42:46 +0700

    feat: callback function

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,30 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, "utf-8", (err, content1) => {
+    const data1 = JSON.parse(content1);
+    const message1 = getLastWord(data1.message);
+
+    fs.readFile(file2, "utf-8", (err, content2) => {
+      const data2 = JSON.parse(content2);
+      const message2 = getLastWord(data2[0].message);
+
+      fs.readFile(file3, "utf-8", (err, content3) => {
+        const data3 = JSON.parse(content3);
+        const message3 = getLastWord(data3[0].data.message);
+
+        fnCallback(err, [message1, message2, message3]);
+      });
+    });
+  });
+};
+
+const getLastWord = (sentence) => {
+  const arrSentence = sentence.split(" ").reverse();
+
+  return arrSentence[0];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
